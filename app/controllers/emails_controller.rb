995a317a1c127a333bb33553e0752a8b09486b25
Ulrichvoitier <ulrichvoitier@yahoo.fr>
2023-03-09 17:24:57 +0100

    "exo"

--- app/controllers/emails_controller.rb
+++ app/controllers/emails_controller.rb
@@ -7,9 +7,18 @@ class EmailsController < ApplicationController
     @emails = Email.create(object: Faker::Lorem.sentence, body: Faker::Lorem.paragraph)
       # redirect_to emails_path
       
-    render turbo_stream: [
-    turbo_stream.prepend('new_email', partial: "message", locals: {emails: @emails})
-    ]
+    # render turbo_stream: [
+    # turbo_stream.prepend('new_email', partial: "message", locals: {emails: @emails})
+    # ]
+    respond_to do |format|
+      if @emails.save
+        format.html { redirect_to emails_path, notice: "Message was successfully created." }
+        format.json { render :show, status: :created, location: @emails }
+      else
+        format.html { render :new, status: :unprocessable_entity }
+        format.json { render json: @emails.errors, status: :unprocessable_entity }
+      end
+    end
   end
 
   def show
